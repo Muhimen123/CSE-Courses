571e78f0f9dba50479bc7d80d6b12c0f278f0b26
Muhimen123 <almuhimen4@gmail.com>
2024-06-24 21:16:49 +0600

    Deque

--- CSE203_204/Queue/Deque_implementation copy.cpp
+++ CSE203_204/Queue/Deque_implementation copy.cpp
@@ -78,6 +78,39 @@ public:
         }
     }
 
+    void push_front(int value) {
+        if(head == nullptr) {
+            push_back(value);
+        } else {
+            Node *new_node = new Node;
+            new_node->value = value;
+            new_node->next = head;
+            head->prev = new_node;
+            head = new_node;
+        }
+    }
+
+    void pop_back() {
+        if(is_empty()) {
+            cout << "Deque is empty" << endl;
+        } else if(head == tail) {
+            pop_front();
+        } else {
+            Node *tmp = tail;
+            tail = tail->prev;
+            tail->next = nullptr;
+            tmp->prev = nullptr;
+            delete tmp;
+        }
+    }
+
+    void back() {
+        if(is_empty()) {
+            cout << "Deque is empty" << endl;
+        } else {
+            cout << tail->value << endl;
+        }
+    }
 };
 
 int main()
@@ -85,11 +118,14 @@ int main()
     Deque q;
     while(true) {
         cout << "Menu: " << endl;
-        cout << "1. Enque" << endl;
+        cout << "1. Push back" << endl;
         cout << "2. Print" << endl;
         cout << "3. Exit" << endl;
         cout << "4. Front" << endl;
-        cout << "5. Dequeue" << endl;
+        cout << "5. Pop front" << endl;
+        cout << "6. Push front" << endl;
+        cout << "7. Pop Back" << endl;
+        cout << "8. Back" << endl;
 
         int choice;
         cin >> choice;
@@ -108,6 +144,15 @@ int main()
             cout << value << endl;
         } else if(choice == 5) {
             q.pop_front();
+        } else if(choice == 6) {
+            int value;
+            cout << "Enter value: ";
+            cin >> value;
+            q.push_front(value);
+        } else if(choice == 7) {
+            q.pop_back();
+        } else if(choice == 8) {
+            q.back();
         }
     }
     return 0;
