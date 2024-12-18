4c0e82970a1a631f1142cdcf0a8c876c45332d50
Muhimen123 <almuhimen4@gmail.com>
2024-12-19 01:40:37 +0600

    Fixed relChar() return type

--- CSE215_216/TRIE_Common_Prefix.cpp
+++ CSE215_216/TRIE_Common_Prefix.cpp
@@ -7,103 +7,95 @@ Sample Problem: https://vjudge.net/problem/SPOJ-ADAINDEX
 using namespace std;
 
 struct Node {
-    bool EoW;
-    int prefix_count;
-    Node *children[26];
+  bool EoW;
+  int prefix_count;
+  Node *children[26];
 
-    Node() {
-        EoW = false;
-        prefix_count = 0;
-        for(int i = 0; i < 26; i++) {
-            children[i] = NULL;
-        }
+  Node() {
+    EoW = false;
+    prefix_count = 0;
+    for (int i = 0; i < 26; i++) {
+      children[i] = NULL;
     }
+  }
 };
 
 class TRIE {
-    private:
-    Node* root;
+private:
+  Node *root;
 
-    int relPos(char ch) {
-        return ch - 'a';
+  int relPos(char ch) { return ch - 'a'; }
+
+  char relChar(int idx) { return idx + 'a'; }
+
+  void printTRIE_(Node *cur, string s = "") {
+    if (cur->EoW == true) {
+      cout << s << endl;
     }
 
-    int relChar(int idx) {
-        return idx + 'a';
+    for (int i = 0; i < 26; i++) {
+      if (cur->children[i] == NULL)
+        continue;
+      char ch = relChar(i);
+      printTRIE_(cur->children[i], s + ch);
+    }
+  }
+
+public:
+  TRIE() { root = new Node(); }
+
+  void insert(string s) {
+    Node *iter = root;
+    for (int i = 0; i < (int)s.size(); i++) {
+      int j = relPos(s[i]);
+
+      if (iter->children[j] == NULL) {
+        iter->children[j] = new Node;
+      }
+      iter->prefix_count++;
+      iter = iter->children[j];
     }
 
-    void printTRIE_(Node* cur, string s = "") {
-        if(cur->EoW == true) {
-            cout << s << endl;
-        }
+    iter->prefix_count++;
+    iter->EoW = true;
+  }
 
-        for(int i = 0; i < 26; i++) {
-            if(cur->children[i] == NULL) continue;
-            char ch = relChar(i);
-            printTRIE_(cur->children[i], s + ch);
-        }
+  bool search(string s) {
+    Node *iter = root;
+    for (int i = 0; i < (int)s.size(); i++) {
+      int j = relPos(s[i]);
+      if (iter->children[j] == NULL)
+        return false;
+      iter = iter->children[j];
     }
 
-    public:
-    TRIE() {
-        root = new Node();
+    return iter->EoW;
+  }
+
+  int get_prefix_count(string pref) {
+    Node *iter = root;
+    for (int i = 0; i < (int)pref.size(); i++) {
+      int j = relPos(pref[i]);
+      if (iter->children[j] == NULL)
+        return 0;
+      iter = iter->children[j];
     }
 
-    void insert(string s) {
-        Node* iter = root;
-        for(int i = 0; i < (int) s.size(); i++) {
-            int j = relPos(s[i]);
+    return iter->prefix_count;
+  }
 
-            if(iter->children[j] == NULL) {
-                iter->children[j] = new Node;
-            }
-            iter->prefix_count++;
-            iter = iter->children[j];
-        }
-
-        iter->prefix_count++;
-        iter->EoW = true;
-    }
-
-    bool search(string s) {
-        Node* iter = root;
-        for(int i = 0; i < (int) s.size(); i++) {
-            int j = relPos(s[i]);
-            if(iter->children[j] == NULL) return false;
-            iter = iter->children[j];
-        }
-
-        return iter->EoW;
-    }
-
-    int get_prefix_count(string pref) {
-        Node* iter = root;
-        for(int i = 0; i < (int) pref.size(); i++) {
-            int j = relPos(pref[i]);
-            if(iter->children[j] == NULL) return 0;
-            iter = iter->children[j];
-        }
-
-        return iter->prefix_count;
-    }
-
-    void printTRIE() {
-        printTRIE_(root, "");
-    }
+  void printTRIE() { printTRIE_(root, ""); }
 };
 
-
-
 int main() {
-    TRIE trie;
-    trie.insert("bobcat");
-    trie.insert("bobcat");
-    trie.insert("bobcat");
-    trie.insert("bobdog");
-    trie.insert("bobbird");
-    trie.printTRIE();
-    int cnt = trie.get_prefix_count("bobcat");
-    cout << cnt << endl;
-    return 0;
-
+  TRIE trie;
+  trie.insert("bobcat");
+  trie.insert("bobcat");
+  trie.insert("bobcat");
+  trie.insert("bobdog");
+  trie.insert("bobbird");
+  trie.printTRIE();
+  int cnt = trie.get_prefix_count("bobcat");
+  cout << cnt << endl;
+  return 0;
 }
